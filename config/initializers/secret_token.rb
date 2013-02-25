3b53330bfbfad2afdc34da956e3f9a9aa3670178
Lawrence Tsang <ltsang@andrew.cmu.edu>
2013-02-25 16:08:57 -0500

    created base ChoreTracker rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChoreTracker::Application.config.secret_token = '96a792559361cc076434e7bf414507c9c0ca8abbf78e433c75d9ccb5684315787c53060bdafd5bba350b2f264d7cf6d73bfec95406fe1cc3368a8f2134cfeeb0'
